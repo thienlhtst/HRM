abd301213564884ceccd6c21308e7a31b3d6f95d
thienlhtst <89084577+thienlhtst@users.noreply.github.com>
2024-03-27 19:54:00 +0700

    up

--- QLNS.WEB/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QLNS.WEB.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- QLNSAdminWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QLNSAdminWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
